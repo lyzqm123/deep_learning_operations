9cf3833767b585375e675ca3346af85c9fb456d1
lyzqm <lyzqm@naver.com>
2022-05-15 13:00:36 +0900

    Add `Conv2D` operation, Add google test. (#1)
    * Add `conv2d.hpp` to cmake
    
    Signed-off-by: lyzqm123 <lyzqm@naver.com>
    
    * Add the `Conv2D` operation
    
    Signed-off-by: lyzqm123 <lyzqm@naver.com>
    
    * Reorganized folder & separated the `CMakeLists.txt`.
    
    Signed-off-by: lyzqm123 <lyzqm@naver.com>
    
    * Add gtest skeleton.
    - Add a `gtest/CMakeLists.txt`
    
    Signed-off-by: lyzqm123 <lyzqm@naver.com>
    
    * Add unittest.
    - Add `conv2d_test`, `quantization_test`
    
    Signed-off-by: lyzqm123 <lyzqm@naver.com>
    
    * Refactoring code.
    
    Signed-off-by: lyzqm123 <lyzqm@naver.com>

---
+++ gtest/conv2d_test.cpp
@@ -0,0 +1,23 @@
+#include "gtest/gtest.h"
+#include "conv2d.hpp"
+
+TEST(conv2dTest, CheckConv2DOutputDimensionIsFine)
+{
+    using TensorType = float;
+    Tensor<TensorType> inputs("inputs",
+                              {3, 3},
+                              {-1.0, -2.0, -3.0, -4.0, -5.0, -6.0, -7.0, -8.0, -9.0});
+    Tensor<TensorType> kernel_3d("kernel_3d", {3, 3}, "ones");
+    Tensor<TensorType> conv2d_3d_3d_output = conv::conv2d(inputs, kernel_3d, "conv2d_3d_3d_output");
+
+    const std::vector<int> conv2d_3d_3d_output_dimension = conv2d_3d_3d_output.get_dimension();
+    const std::vector<int> conv2d_3d_3d_output_dimension_answer = {1, 1};
+    EXPECT_EQ(conv2d_3d_3d_output_dimension, conv2d_3d_3d_output_dimension_answer);
+
+
+    Tensor<TensorType> kernel_1d("kernel_1d", {1, 1}, "ones");
+    Tensor<TensorType> conv2d_3d_1d_output = conv::conv2d(inputs, kernel_1d, "conv2d_3d_1d_output");
+    const std::vector<int> conv2d_3d_1d_output_dimension = conv2d_3d_1d_output.get_dimension();
+    const std::vector<int> conv2d_3d_1d_output_dimension_answer = {3, 3};
+    EXPECT_EQ(conv2d_3d_1d_output_dimension, conv2d_3d_1d_output_dimension_answer);
+}
