1634b6d5ce40638ad811ba1091f767996462eecd
lyzqm123 <lyzqm@naver.com>
2022-06-01 16:32:23 +0900

    Add quantized_tensor copy consturcotr.
    Signed-off-by: lyzqm123 <lyzqm@naver.com>

--- src/tensor/quantized_tensor.hpp
+++ src/tensor/quantized_tensor.hpp
@@ -11,7 +11,16 @@ public:
     QuantizedTensor(std::string name, std::vector<int> dimension, std::string distribution_type) : Tensor<DQ>(name, dimension, distribution_type) {}
     QuantizedTensor(std::string name, std::vector<int> dimension, const std::vector<DQ> &init_tensor) : Tensor<DQ>(name, dimension, init_tensor) {}
     QuantizedTensor(std::string name, std::initializer_list<int> dimension, const std::initializer_list<DQ> &init_tensor) : Tensor<DQ>(name, dimension, init_tensor) {}
+    QuantizedTensor(const QuantizedTensor &other) : QuantizedTensor(other.name_, other.dimension_) 
+    {
+		int other_dimension_size = other.dimension_[0] * other.dimension_[1] * other.dimension_[2] * other.dimension_[3];
+		std::copy(other.tensor_, other.tensor_ + other_dimension_size, this->tensor_);
 
+        this->scale_ = other.scale_;
+        this->offset_ = other.offset_;
+    }
+
+    ~QuantizedTensor() {}
     void set_scale(Q scale)
     {
         this->scale_ = scale;
